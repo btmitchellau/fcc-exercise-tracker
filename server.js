6f6e983b51732c05a0c52b08637e299075cedf5f
btmitchell@hotmail.com <btmitchell@hotmail.com>
2020-09-21 15:02:47 +0930

    code cleanup, eslint etc

--- server.js
+++ server.js
@@ -1,162 +1,160 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const db = require('./db.js');
+const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const db = require('./db.js');
+
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false,
 }));
 app.use(bodyParser.json());
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(`${__dirname}/views/index.html`);
 });
 
 app.post('/api/exercise/new-user', (req, res) => {
-
-    // check if it exists  
-    db.User.findOne({
-        "name": req.body.username
-    }, (err, userFound) => {
-
-        if (err) {
-            console.log('error' + err);
-
-        } else if (userFound == null) {
-            const newUser = db.User({
-                "username": req.body.username
-            });
-            newUser.save((err, data) => {
-                res.json({
-                    "username": req.body.username,
-                    "_id": data.id
-                });
-            });
-
-        } else {
-            res.json({
-                "error": 'user already exists!'
-            });
-        }
-    });
+  // Check the user exists...
+  db.User.findOne({
+    name: req.body.username,
+  }, (err, userFound) => {
+    if (err) {
+      res.json({
+        error: 'Error checking for user',
+      });
+    } else if (userFound == null) { // create if it doesn't...
+      const newUser = db.User({
+        username: req.body.username,
+      });
+      newUser.save((_err, data) => {
+        res.json({
+          username: req.body.username,
+          _id: data.id,
+        });
+      });
+    } else {
+      res.json({
+        error: 'user already exists!',
+      });
+    }
+  });
 });
 
 app.get('/api/exercise/users', (req, res) => {
-    query = db.User.find({}).select('username __id');
-    query.exec((err, data) => {
-        res.json(data);
-    });
+  const query = db.User.find({}).select('username __id');
+  query.exec((err, users) => {
+    res.json(users);
+  });
 });
 
 app.post('/api/exercise/add', (req, res) => {
+  let theDate = new Date().toDateString();
+  if (req.body.date !== undefined) {
+    theDate = new Date(req.body.date).toDateString();
+  }
 
-    let theDate = req.body.date;
-    theDate = new Date().toDateString();
-    if (req.body.date != undefined) {
-        theDate = new Date(req.body.date).toDateString();
-    };
+  const update = {
+    userId: req.body.userId,
+    description: req.body.description,
+    duration: parseInt(req.body.duration, 10),
+    date: theDate,
+  };
 
-    const update = {
-        "userId": req.body.userId,
-        "description": req.body.description,
-        "duration": parseInt(req.body.duration),
-        "date": theDate
-    };
+  db.User.updateOne({
+    _id: req.body.userId,
+  }, {
+    $addToSet: {
+      exercises: [update],
+    },
+  },
+  (err) => {
+    if (err) {
+      res.send(err);
+    } else {
+      const query = db.User.find({
+        _id: req.body.userId,
+      });
+      query.exec((_err, user) => {
+        const response = {
+          username: user[0].username,
+          description: update.description,
+          duration: parseInt(update.duration, 10),
+          _id: req.body.userId,
+          date: update.date,
+        };
 
-    db.User.updateOne({
-            _id: req.body.userId
-        }, {
-            $addToSet: {
-                exercises: [update]
-            }
-        },
-        (err, result) => {
-            if (err) {
-                res.send(err);
-            } else {
-
-                query = db.User.find({
-                    "_id": req.body.userId
-                });
-                query.exec((err, user) => {
-
-                    const response = {
-                        "username": user[0].username,
-                        "description": update.description,
-                        "duration": parseInt(update.duration),
-                        "_id": req.body.userId,
-                        "date": update.date
-                    };
-
-                    res.json(response)
-                });
-            }
-        });
+        res.json(response);
+      });
+    }
+  });
 });
 
 app.get('/api/exercise/log', (req, res) => {
+  db.User.findOne({
+    _id: req.query.userId,
+  }).exec((err, user) => {
+    if (err) {
+      res.send(err);
+    } else if (!user) {
+      res.json({
+        error: 'user not found'
+      });
+    } else {
+      let filteredExercises = user.exercises.toObject();
+      if (req.query.from !== undefined) {
+        filteredExercises = filteredExercises.filter((exercise) => {
+          return exercise.date.getTime() > new Date(req.query.from).getTime();
+        });
+      }
 
-    db.User.findOne({
-        "_id": req.query.userId
-    }).exec((err, user) => {
-        if (err) {} else if (!user) {} else {
+      if (req.query.from !== undefined) {
+        filteredExercises = filteredExercises.filter((exercise) => {
+          return exercise.date.getTime() > new Date(req.query.from).getTime();
+        });
+      }
 
-            let filteredExercises = user.exercises.toObject();
-            if (req.query.from !== undefined) {
-                filteredExercises = filteredExercises.filter((exercise) => {
-                    return exercise.date.getTime() > new Date(req.query.from).getTime();
-                });
-            };            
+      if (req.query.limit !== undefined) {
+        filteredExercises = filteredExercises.slice(0, req.query.limit);
+      }
 
-            if (req.query.from !== undefined) {
-                filteredExercises = filteredExercises.filter((exercise) => {
-                    return exercise.date.getTime() > new Date(req.query.from).getTime();
-                });
-            };
-
-            if (req.query.limit !== undefined) {
-                filteredExercises = filteredExercises.slice(0, req.query.limit);
-            };
-
-            res.json({
-                "user": user.username,
-                "log": filteredExercises,
-                "count": filteredExercises.length
-            });
-        }
-    })
+      res.json({
+        user: user.username,
+        log: filteredExercises,
+        count: filteredExercises.length,
+      });
+    }
+  });
 });
 
 // Not found middleware
-app.use((req, res, next) => {
-    return next({
-        status: 404,
-        message: 'not found'
-    })
-})
+app.use((req, res, next) => next({
+  status: 404,
+  message: 'not found',
+}));
 
 // Error Handling middleware
-app.use((err, req, res, next) => {
-    let errCode, errMessage
+app.use((err, req, res) => {
+  let errCode;
+  let errMessage;
 
-    if (err.errors) {
-        // mongoose validation error
-        errCode = 400 // bad request
-        const keys = Object.keys(err.errors)
-        // report the first validation error
-        errMessage = err.errors[keys[0]].message
-    } else {
-        // generic or custom error
-        errCode = err.status || 500
-        errMessage = err.message || 'Internal Server Error'
-    }
-    res.status(errCode).type('txt')
-        .send(errMessage)
-})
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message;
+  } else {
+    // generic or custom error
+    errCode = err.status || 500;
+    errMessage = err.message || 'Internal Server Error';
+  }
+  res.status(errCode).type('txt')
+    .send(errMessage);
+});
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-    console.log('Your app is listening on port ' + listener.address().port)
-})
+app.listen(process.env.PORT || 3000, () => {
+  //  console.log(`Your app is listening on port ${listener.address().port}`);
+});
